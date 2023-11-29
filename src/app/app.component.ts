bafd88a5b1542860fec3207d34de76b100a50861
Gregory Marsh <mises60115@gmail.com>
2023-11-29 14:41:25 -0500

    Generated housing location component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports: [HomeComponent],
   template: `
         <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
   `,
   styleUrls: ['./app.component.css'],
 })
