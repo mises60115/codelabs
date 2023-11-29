6a437977173856ce7a6bdf4e432b90363ff4c6a3
Gregory Marsh <mises60115@gmail.com>
2023-11-29 15:51:03 -0500

    List of housing locations displays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { HousingLocationComponent } from './housing-location/housing-location.component';
+
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HomeComponent, HousingLocationComponent],
+  imports: [HomeComponent],
   template: `
         <header class="brand-name">
           <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
       <section class="content">
         <app-home></app-home>
-      </section>
-      <section class="results">
-        <app-housing-location></app-housing-location>
       </section>
   `,
   styleUrls: ['./app.component.css'],
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,13 +8,15 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img class="listing-photo">
-      <h2 class="listing-heading"></h2>
-      <p class="listing-location"></p>
+      <img class="listing-photo" [src]="locFromHousingLocCmpnt.photo" alt="Exterior photo of {{ locFromHousingLocCmpnt.name }}">
+      <h2 class="listing-heading">{{ locFromHousingLocCmpnt.name }}</h2>
+      <p class="listing-location">{{ locFromHousingLocCmpnt.city }}, {{ locFromHousingLocCmpnt.state }}</p>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  /* ! estimation point or non-null assertion operator
+  tells typescript the property will not be undefined */
+  @Input() locFromHousingLocCmpnt!: HousingLocation;
 }
