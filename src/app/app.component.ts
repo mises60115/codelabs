e86c51c512a12abb08a1c8ce8e16aa56a0014561
Gregory Marsh <mises60115@gmail.com>
2023-11-29 14:20:35 -0500

    Generated home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `
+        <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
