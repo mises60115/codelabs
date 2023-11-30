d65c628460a2ffa1fb58914462f9a79c1a23d556
Gregory Marsh <mises60115@gmail.com>
2023-11-30 17:38:03 -0500

    Completed part 6 housing service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,23 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works! {{ detailsLocId }}
-    </p>
+  <article>
+    <img class="listing-photo" [src]="detailsHousingLoc?.photo" alt="Exterior photo of {{ detailsHousingLoc?.name }}">
+    <section class="listing-description">
+      <h2 class="listing-heading">{{ detailsHousingLoc?.name }}</h2>
+      <p class="listing-location">{{ detailsHousingLoc?.city }}, {{ detailsHousingLoc?.state }}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{ detailsHousingLoc?.availableUnits }}</li>
+        <li>Does this location have wifi: {{ detailsHousingLoc?.wifi }}</li>
+        <li>Does this location have laundry: {{ detailsHousingLoc?.laundry }}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary" type="button" >Apply now</button>
+    </section>
+  </article>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
   detailsRoute: ActivatedRoute = inject(ActivatedRoute);
-  detailsLocId = 0;
+  detailsHousingLoc: HousingLocation | undefined;
+  detailsHousingSvc: HousingService = inject(HousingService);
 
   constructor() {
-    this.detailsLocId = Number(this.detailsRoute.snapshot.params['id']);
+    const detailsLocId = Number(this.detailsRoute.snapshot.params['id']);
+    this.detailsHousingLoc = this.detailsHousingSvc.getHousingLocationById(detailsLocId);
   }
 }
