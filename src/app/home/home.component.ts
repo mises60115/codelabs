b3c4ea0aef1cc7d8c201d3377444836c17c5ebe4
Gregory Marsh <mises60115@gmail.com>
2023-12-01 17:19:56 -0500

    Part 8 complete: Added city filtering on main page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,24 +10,37 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filterInputElem />
+        <button class="primary" type="button" (click)="filterResults(filterInputElem.value)">Search</button>
       </form>
     </section>
     <section class="results">
-        <app-housing-location *ngFor="let theLoc of homeCmpntLocList" [locationOfHousingLocCmpnt]="theLoc"></app-housing-location>
+        <app-housing-location *ngFor="let theLoc of homeCmpntFilteredLocList" [locationOfHousingLocCmpnt]="theLoc"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
   imports: [CommonModule, HousingLocationComponent]
 })
 export class HomeComponent {
-  homeCmpntLocList: HousingLocation[] = [];
+
+  homeCmpntAllLocList: HousingLocation[] = [];
   homeCmpntHousingSvc: HousingService = inject(HousingService);
+  homeCmpntFilteredLocList: HousingLocation[] = [];
 
   constructor() {
-    this.homeCmpntHousingSvc.getAllHousingLocations().then((locList: HousingLocation[]) => { this.homeCmpntLocList = locList });
+    this.homeCmpntHousingSvc.getAllHousingLocations().then((locList: HousingLocation[]) => {
+      this.homeCmpntAllLocList = locList;
+      this.homeCmpntFilteredLocList = locList
+    });
+
 
     //this.locListFromHomeCmpnt = this.housingSvcFromHomeCmpnt.getAllHousingLocations();
+  }
+
+  filterResults(filterInputText: string) {
+    if (!filterInputText) {
+      this.homeCmpntFilteredLocList = this.homeCmpntAllLocList;
+    }
+    this.homeCmpntFilteredLocList = this.homeCmpntAllLocList.filter(theLoc => theLoc?.city.toLowerCase().includes(filterInputText.toLowerCase()));
   }
 }
